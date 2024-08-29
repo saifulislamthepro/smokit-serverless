c7d369988de5f4bdd63184eef62522865d3980e9
Saiful Islam <mdsaifulislamsaiftech@gmail.com>
2024-08-29 23:59:17 +0600

    example

--- api/index.js
+++ api/index.js
@@ -1,40 +1,4 @@
-const express = require('express');
-require('dotenv').config();
-const connectDB = require('../config/db');
-const pageRoutes = require('./pageRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
-const cartRoutes = require("./cartRoutes");
-const authRoutes = require('./authRoutes');
-const orderRoutes = require('./orderRoutes');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const session = require("express-session");
-
-const app = express();
-
-connectDB();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({resave:false, saveUninitialized:false, secret: "my_secret"}));
-
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Static folder
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'uploads')));
-
-
-
-// Serve pages
-app.use( pageRoutes);
-app.use( dashboardRoutes);
-app.use( cartRoutes);
-app.use( authRoutes);
-app.use( orderRoutes);
-
-module.exports = app;
+module.exports = (req, res) => {
+    res.status(200).json({ message: "Hello, Vercel!" });
+  };
+  
--- api/routes/pageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getHomePage, getProductPage } = require('../../controllers/pageController');
-
-const router = express.Router();
-
-router.get('/', getHomePage);
-router.get('/product/:id', getProductPage);
-
-module.exports = router;
