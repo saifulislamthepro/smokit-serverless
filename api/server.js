d7cfb222947c406a8ff49f83c162a8aa0053d92e
Saiful Islam <mdsaifulislamsaiftech@gmail.com>
2024-08-29 21:24:11 +0600

    export module

--- api/server.js
+++ api/server.js
@@ -37,7 +37,8 @@ app.use( cartRoutes);
 app.use( authRoutes);
 app.use( orderRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
+module.exports = (req, res) => {
+    return new Promise((resolve) => {
+      app(req, res, resolve);
+    });
+  };
